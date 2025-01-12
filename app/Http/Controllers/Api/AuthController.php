2c58ce4118d4ce9adc76ae7bb2d2c0f20cf17fdf
mouafo <hari.randoll@gmail.com>
2025-01-12 14:02:00 +0100

    add auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use App\Models\User;
+use App\Mail\ResetPasswordMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Identifiants invalides'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,22 @@ use App\Http\Controllers\Api\FlightController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\ReservationController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('test', function () {
 
 
-return 'okok';
+    return 'okok';
+});
 
- });
+
+
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
 
 Route::resource('users', UserController::class);
 Route::resource('flights', FlightController::class);
 Route::resource('payments', PaymentController::class);
 Route::resource('reservations', ReservationController::class);
-
-
-
